312d9718c6f3b8522b58b7627ace8d2fb9cc3cd8
Riteshsatwe <144507879+Riteshsatwe@users.noreply.github.com>
2024-10-07 17:31:32 +0530

    Initial commit

---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              SIOMMCA-2024
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
